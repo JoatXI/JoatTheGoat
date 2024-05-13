cafef78b421772d1217e819b355a811c315af4a2
Joat <michaelpaterson194@gmail.com>
2024-05-14 00:27:14 +0100

    REF SECTION UPDATE:
    Modified the Ref page by changing the container tag to a div.
    Created 3 div with a className: marquee that contains the Attribution contents.
    Created each Attribution content as a list item within and unordered list marquee-content.
    Added a useEffect to help implement the duplication effect of the marquee content.
    This is donme by getting the specified amount of marquee-elements to be dispalyed, appending them into an array, looping through & duplicating them.
    This is done to ensure the marquee type effect is done on each list item without displaying any blank spaced a they slide through the screen.
    Added the appropriate styling and animation effect using CSS animation.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './App.css';
 import { NavBar } from './components/NavBar';
 import { Banner } from './components/Banner';
--- src/components/Ref.js
+++ src/components/Ref.js
@@ -1,23 +1,60 @@
 import { Container } from "react-bootstrap";
+import { useEffect } from "react";
 
 export const Ref = () => {
+
+    useEffect(() => {
+        const root = document.documentElement;
+        const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
+        const marqueeContent = document.querySelector(".marquee-content");
+        const marqueeTwo = document.querySelector(".n1");
+        const marqueeThree = document.querySelector(".n2");
+
+        if (marqueeContent) {
+            root.style.setProperty("marquee-elements", marqueeContent.children.length);
+            root.style.setProperty("marquee-elements", marqueeTwo.children.length);
+            root.style.setProperty("marquee-elements", marqueeThree.children.length);
+            
+            for (let i = 0; i < marqueeElementsDisplayed; i++) {
+                marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+                marqueeTwo.appendChild(marqueeTwo.children[i].cloneNode(true));
+                marqueeThree.appendChild(marqueeThree.children[i].cloneNode(true));
+            }
+        }
+    }, []);
 
     return (
         <section className="ref" id="ref">
             <Container fluid>
-                <div className="ref-box">
-                    <marquee>
-                        <h4 className="ref-scroller">
-                            <span className="ref-head">Attributions: </span>
-                            <span className="ref-text">Icons By Flaticon</span>
-                            <span className="ref-text">Photos By Pexels</span>
-                        </h4>
-                        <h4 className="ref-scroller">
-                        <span className="ref-head">Attributions: </span>
-                            <span className="ref-text">Icons By Flaticon</span>
-                            <span className="ref-text">Photos By Pexels</span>
-                        </h4>
-                    </marquee>
+                <div className="marquee">
+                    <ul className="marquee-content">
+                        <li className="ref-head">Attributions: </li>
+                        <li className="ref-text">Icons By Flaticon</li>
+                        <li className="ref-text">Photos By Pexels</li>
+                        <li className="ref-head">Inspiration: </li>
+                        <li className="ref-text">Vishwa</li>
+                        <li className="ref-text">WD-Code YT</li>
+                    </ul>
+                </div>
+                <div className="marquee">
+                    <ul className="marquee-content n1">
+                        <li className="ref-head">Attributions: </li>
+                        <li className="ref-text">Icons By Flaticon</li>
+                        <li className="ref-text">Photos By Pexels</li>
+                        <li className="ref-head">Inspiration: </li>
+                        <li className="ref-text">Vishwa</li>
+                        <li className="ref-text">WD-Code YT</li>
+                    </ul>
+                </div>
+                <div className="marquee">
+                    <ul className="marquee-content n2">
+                        <li className="ref-head">Attributions: </li>
+                        <li className="ref-text">Icons By Flaticon</li>
+                        <li className="ref-text">Photos By Pexels</li>
+                        <li className="ref-head">Inspiration: </li>
+                        <li className="ref-text">Vishwa</li>
+                        <li className="ref-text">WD-Code YT</li>
+                    </ul>
                 </div>
             </Container>
         </section>
